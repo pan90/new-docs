37bee7673b98239a217f09e741777ee1b3251701
Paper99 <1398872296@qq.com>
2024-05-22 17:05:47 +0800

    第一次提交，做的不太好

---
+++ .vitepress/config.ts
@@ -0,0 +1,69 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "PaperCard文档",
+  description: "纸片PaperCard服务器详细文档",
+  lang: 'zh-CN',
+  cleanUrls: true,
+  metaChunk: true,
+  lastUpdated: true,
+
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+
+  themeConfig: {
+
+    search: {
+      provider: 'local',
+    },
+
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '官网', link: 'https://paper-card.cn' },
+      { text: '文档', link: '/player-commands' }
+    ],
+
+    sidebar: [
+      {
+        text: '玩家指令大全',
+        link: "/player-commands"
+      }
+    ],
+
+    // socialLinks: [
+    //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    // ],
+
+    // editLink: {
+    //   pattern: 'https://github.com/NapNeko/NapCatDocs/tree/main/docs/:path',
+    //   text: '在 GitHub 上编辑此页面'
+    // },
+
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+
+    outline: {
+      label: '页面导航'
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium'
+      }
+    },
+
+    langMenuLabel: '多语言',
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式'
+  }
+})
---
+++ index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "纸片PaperCard"
+  text: "一个有趣的Minecraft服务器"
+  tagline: 正版公益 原版生存
+  actions:
+    - theme: brand
+      text: 马上加入
+      link: https://paper-card.cn/join
+    - theme: alt
+      text: 文档介绍
+      link: /player-commands
+
+features:
+  - title: 正版公益
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 原版生存
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 丰富插件
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
